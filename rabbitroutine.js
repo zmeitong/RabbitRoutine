090cb57f67231c633a2dae612a6fb6f77fae8444
albrightn <88120544+albrightn@users.noreply.github.com>
2023-01-15 12:04:45 -0500

    Add files via upload
    Signed-off-by: albrightn <88120544+albrightn@users.noreply.github.com>

---
+++ rabbitroutine.js
@@ -0,0 +1,20 @@
+function createCourse(){
+
+    var x = document.getElementById("courseForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+    
+}
+
+function create_Event(){
+    var x = document.getElementById("eventForm");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+}
+
