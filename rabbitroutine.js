cbf9f30de2deb5d65522eb359cfbfdd66b547ac3
zmeitong <118071440+zmeitong@users.noreply.github.com>
2023-01-15 14:01:47 -0500

    Update rabbitroutine.js
    Signed-off-by: zmeitong <118071440+zmeitong@users.noreply.github.com>

--- rabbitroutine.js
+++ rabbitroutine.js
@@ -2,41 +2,42 @@ var interfaceDisplays = document.getElementsByClassName("speechbox")
 
 function displayHome(){
     var x = document.getElementById("homepage");
+   document.getElementById("rabbit").src= 'https://i.imgur.com/iPrbaod.png';
+  
     if (x.style.zIndex == 0) {
           for (var i = 0; i < interfaceDisplays.length; i++){
             interfaceDisplays[i].style.zIndex=0;};
          x.style.zIndex = 1;
     }
-    console.log("homepage", x.style.zIndex)
- 
+    
 }
 
 function create_Course(){
     var x = document.getElementById("newCoursePage");
+     document.getElementById("rabbit").src= 'https://i.imgur.com/iPrbaod.png';
     if (x.style.zIndex == 0) {
       for (var i = 0; i < interfaceDisplays.length; i++){
         interfaceDisplays[i].style.zIndex=0;};
      x.style.zIndex = 1;
     }
-    console.log("create course page", x.style.zIndex)
 }
 
 function displayCourse(){
     var x = document.getElementById("courseresults");
+    document.getElementById("rabbit").src= 'https://i.imgur.com/KvtfXtt.png';
     if (x.style.zIndex == 0) {
       for (var i = 0; i < interfaceDisplays.length; i++){
         interfaceDisplays[i].style.zIndex=0;};
      x.style.zIndex = 1;
     }
-    console.log("course results page", x.style.zIndex)
 }
 
 function create_Event(){
     var x = document.getElementById("newEventPage");
+    document.getElementById("rabbit").src= 'https://i.imgur.com/iPrbaod.png';
     if (x.style.zIndex == 0) {
           for (var i = 0; i < interfaceDisplays.length; i++){
             interfaceDisplays[i].style.zIndex=0;};
          x.style.zIndex = 1;
     }
-  console.log("new event page", x.style.zIndex)
 }
